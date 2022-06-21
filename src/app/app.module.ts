e9a26ccf6b32c6552ca457fa59ba8583344faab9
Jingni Zhou <jingni.zhou@accenture.com>
2022-06-21 13:28:44 +0800

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { UserCenterComponent } from './user-center/user-center.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RouterModule } from '@angular/router';
+
+const routes=[
+  {path:'',component:IndexComponent},
+  {path:'index',component:IndexComponent},
+  {path:'user/center',component:UserCenterComponent},
+  {path:'**',component:NotFoundComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    UserCenterComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
